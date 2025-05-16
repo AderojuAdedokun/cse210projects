793d1aa04c9b1b42ffc8ac6f74a06eddda7b6210
Aderoju Adedokun <cadedokun@byupathway.edu>
2025-05-16 08:57:06 +0100

    commit

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        // To be implemented
+    }
+
+    public void DisplayAll()
+    {
+        // To be implemented
+    }
+
+    public void SaveToFile(string file)
+    {
+        // To be implemented
+    }
+
+    public void LoadFromFile(string file)
+    {
+        // To be implemented
+    }
+}
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    private List<string> _prompts = new List<string>();
+
+    public string GetRandomPrompt()
+    {
+        // To be implemented
+        return "";
+    }
+}
