3c23b5d7c24078838a990fba37e8f55e05d5777a
Aderoju Adedokun <cadedokun@byupathway.edu>
2025-06-18 06:16:22 +0100

    commit

---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,26 @@
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        // Each lap is 50 meters. Convert to miles: meters * 0.000621371
+        return _laps * 50 * 0.000621371;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+}
